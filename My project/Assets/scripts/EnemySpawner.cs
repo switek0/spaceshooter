b88476276c319a5129e51c35e09af89710ab773a
switek0 <msswitkow@gmail.com>
2024-11-24 12:32:35 +0100

    .

--- My project/Assets/scripts/EnemySpawner.cs
+++ My project/Assets/scripts/EnemySpawner.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemySpawner : MonoBehaviour
 {
+   public GameObject enemyPrefab;
+
+    public float spawnrate = 2f;
+
+    public float minXAispawnValue;
+    public float maxXAispawnValue;
+
+    public float yAXisSpawnValue = 4f;
+
+    private float timeSinceLastAction = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Al=
     }
 
     // Update is called once per frame
@@ -15,4 +27,11 @@ public class EnemySpawner : MonoBehaviour
     {
         
     }
+    void SpawnEnemy()
+    {
+        float xPosition = Random.Range(minXAispawnValue, maxXAispawnValue);
+        Vector3 spawnPosition = new Vector3(xPosition, yAxisSpawnValue);
+    }
+
+
 }
